568e1d91709a03a9fa9276273d11eb64fd5c53d1
new <tiwaripriyanshu640@gmail.com>
2026-07-01 23:45:19 +0530

    salon payloadDto

--- salon service/salon-service/src/main/java/com/rancho/service/SalonService.java
+++ salon service/salon-service/src/main/java/com/rancho/service/SalonService.java
@@ -4,6 +4,19 @@ import com.rancho.modal.Salon;
 import com.rancho.payload.dto.SalonDTO;
 import com.rancho.payload.dto.UserDTO;
 
+import java.util.List;
+
 public interface SalonService {
+
     Salon createSalon(SalonDTO salon, UserDTO user);
+
+    Salon updateSalon(SalonDTO salon, UserDTO user,Long salonId);
+
+    List<Salon> getAllSalons();
+
+    Salon getSalonById(Long salonId);
+
+    Salon getSalonByOwnerId(Long ownerId);
+
+    List<Salon> searchSalonByCity(String city);
 }
