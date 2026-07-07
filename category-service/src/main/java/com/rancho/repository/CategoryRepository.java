27f0cd336b338d507fdd63409fd13b5e2d9ac272
new <tiwaripriyanshu640@gmail.com>
2026-07-07 22:44:13 +0530

    category Repo

--- category-service/src/main/java/com/rancho/repository/CategoryRepository.java
+++ category-service/src/main/java/com/rancho/repository/CategoryRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Set;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 
-    List<Category> findBySalonId(Long salonId);
+    Set<Category> findBySalonId(Long salonId);
 }
--- category-service/src/main/java/com/rancho/service/impl/CategoryServiceImpl.java
+++ category-service/src/main/java/com/rancho/service/impl/CategoryServiceImpl.java
@@ -2,30 +2,39 @@ package com.rancho.service.impl;
 
 import com.rancho.dto.SalonDTO;
 import com.rancho.modal.Category;
+import com.rancho.repository.CategoryRepository;
 import com.rancho.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
-
+    private final CategoryRepository categoryRepository;
 
 
     @Override
     public Category saveCategory(Category category, SalonDTO salonDTO) {
-        return null;
+        Category newCategory=new Category();
+        newCategory.setName(category.getName());
+        newCategory.setSalonId(salonDTO.getId());
+        newCategory.setImage(category.getImage());
+
+        return categoryRepository.save(newCategory);
     }
 
     @Override
     public Set<Category> getAllCategoryBySalon(Long id) {
-        return Set.of();
+        return categoryRepository.findBySalonId(id);
     }
 
     @Override
     public Category getCategoryById(Long id) {
-        return null;
+        Category category=categoryRepository
+        return ;
     }
 
     @Override
