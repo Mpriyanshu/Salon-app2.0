8c5efaf402561dd9ff1cc87bea09c403fc85912a
new <tiwaripriyanshu640@gmail.com>
2026-07-09 23:01:47 +0530

    category Repo

--- category-service/src/main/java/com/rancho/controller/SalonCategoryController.java
+++ category-service/src/main/java/com/rancho/controller/SalonCategoryController.java
@@ -21,7 +21,20 @@ public class SalonCategoryController {
 
         SalonDTO salonDTO=new SalonDTO();
         salonDTO.setId(1L);
-        Category categories=categoryService.saveCategory(category,salonDTO);
-        return ResponseEntity.ok(categories);
+
+        Category savedCategory=categoryService.saveCategory(category,salonDTO);
+
+        return ResponseEntity.ok(savedCategory);
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) throws Exception {
+
+        SalonDTO salonDTO=new SalonDTO();
+        salonDTO.setId(1L);
+
+        categoryService.deleteCategoryById(id,salonDTO.getId());
+
+        return ResponseEntity.ok("category deleted successfully");
     }
 }
