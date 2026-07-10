2e6a6a34e3b2fe1cd2a34f7a3b1b6781f47dd7e0
new <tiwaripriyanshu640@gmail.com>
2026-07-10 22:19:05 +0530

    services dto

--- service offering/service-offering/src/main/java/com/rancho/service/ServiceOfferingService.java
+++ service offering/service-offering/src/main/java/com/rancho/service/ServiceOfferingService.java
@@ -5,9 +5,18 @@ import com.rancho.dto.SalonDTO;
 import com.rancho.dto.ServiceDTO;
 import com.rancho.modal.ServiceOffering;
 
+import java.util.List;
+import java.util.Set;
+
 public interface ServiceOfferingService {
 
     ServiceOffering createService(SalonDTO salonDTO,
                                   ServiceDTO serviceDTO,
                                   CategoryDTO CategoryDTO);
+
+    ServiceOffering updateOffering(Long serviceId, ServiceOffering service);
+
+    Set<ServiceOffering>  getAllServiceBySalonId(Long salonId,Long catyegoryId);
+
+    Set<ServiceOffering> getServicesById(Set<Long> ids);
 }
