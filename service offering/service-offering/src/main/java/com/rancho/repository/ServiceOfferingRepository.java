270b78d1b505fdea72621450bc2194fc1486f73e
new <tiwaripriyanshu640@gmail.com>
2026-07-11 23:18:30 +0530

    services repo pack

--- service offering/service-offering/src/main/java/com/rancho/repository/ServiceOfferingRepository.java
+++ service offering/service-offering/src/main/java/com/rancho/repository/ServiceOfferingRepository.java
@@ -3,5 +3,10 @@ package com.rancho.repository;
 import com.rancho.modal.ServiceOffering;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface ServiceOfferingRepository extends JpaRepository<ServiceOffering,Long> {
+
+    Set<ServiceOffering> findBySalonId(Long salonId);
 }
