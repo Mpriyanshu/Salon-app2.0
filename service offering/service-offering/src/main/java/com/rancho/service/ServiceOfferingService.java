b859b2f73f52613514a44ea532e20d50d71604ea
new <tiwaripriyanshu640@gmail.com>
2026-07-12 22:38:50 +0530

    services impl finish

--- service offering/service-offering/src/main/java/com/rancho/service/ServiceOfferingService.java
+++ service offering/service-offering/src/main/java/com/rancho/service/ServiceOfferingService.java
@@ -10,13 +10,15 @@ import java.util.Set;
 
 public interface ServiceOfferingService {
 
-    ServiceOffering createService(SalonDTO salonDTO,
+    ServiceOffering createService(SalonDTO salonDto,
                                   ServiceDTO serviceDTO,
-                                  CategoryDTO CategoryDTO);
+                                  CategoryDTO categoryDTO);
 
     ServiceOffering updateOffering(Long serviceId, ServiceOffering service) throws Exception;
 
-    Set<ServiceOffering>  getAllServiceBySalonId(Long salonId,Long catyegoryId);
+    Set<ServiceOffering>  getAllServiceBySalonId(Long salonId,Long categoryId);
 
     Set<ServiceOffering> getServicesById(Set<Long> ids);
+
+    ServiceOffering getServiceById(Long id) throws Exception;
 }
