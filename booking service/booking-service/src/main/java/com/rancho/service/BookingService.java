5849352b52d28e45a373cb8a11d5404c1fc7dee7
new <tiwaripriyanshu640@gmail.com>
2026-07-20 21:42:41 +0530

    booking Domain

--- booking service/booking-service/src/main/java/com/rancho/service/BookingService.java
+++ booking service/booking-service/src/main/java/com/rancho/service/BookingService.java
@@ -1,11 +1,14 @@
 package com.rancho.service;
 
+import com.rancho.domain.BookingStatus;
 import com.rancho.dto.BookingRequest;
 import com.rancho.dto.SalonDTO;
 import com.rancho.dto.ServiceDTO;
 import com.rancho.dto.UserDTO;
 import com.rancho.modal.Booking;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 public interface BookingService {
@@ -14,4 +17,10 @@ public interface BookingService {
                            UserDTO ser,
                            SalonDTO salonDTO,
                            Set<ServiceDTO> serviceDTOSet);
+
+     List<Booking> getBookingsByCustomer(Long customerId);
+     List<Booking> getBookingBySalon(Long salonId);
+     Booking getBooKingById(Long Id);
+     Booking UpdateBooking(Long bookingId, BookingStatus status);
+     List<Booking> getBookingsByDate(LocalDate date, Long salonId);
 }
