7ffe21e9a60a1c4baa54fd24cc258d32b1bdfd45
new <tiwaripriyanshu640@gmail.com>
2026-07-20 22:06:58 +0530

    booking serviceimpl

---
+++ booking service/booking-service/src/main/java/com/rancho/service/impl/BookingServiceImpl.java
@@ -0,0 +1,53 @@
+package com.rancho.service.impl;
+
+import com.rancho.domain.BookingStatus;
+import com.rancho.dto.BookingRequest;
+import com.rancho.dto.SalonDTO;
+import com.rancho.dto.ServiceDTO;
+import com.rancho.dto.UserDTO;
+import com.rancho.modal.Booking;
+import com.rancho.modal.SalonReport;
+import com.rancho.service.BookingService;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+public class BookingServiceImpl implements BookingService {
+
+
+    @Override
+    public Booking createBooking(BookingRequest booking, UserDTO ser, SalonDTO salonDTO, Set<ServiceDTO> serviceDTOSet) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getBookingsByCustomer(Long customerId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Booking> getBookingBySalon(Long salonId) {
+        return List.of();
+    }
+
+    @Override
+    public Booking getBooKingById(Long Id) {
+        return null;
+    }
+
+    @Override
+    public Booking UpdateBooking(Long bookingId, BookingStatus status) {
+        return null;
+    }
+
+    @Override
+    public List<Booking> getBookingsByDate(LocalDate date, Long salonId) {
+        return List.of();
+    }
+
+    @Override
+    public SalonReport getSalonReport(Long salonId) {
+        return null;
+    }
+}
