08f8b17d22c64294fc7fc1ebcbc28dd223156cd0
new <tiwaripriyanshu640@gmail.com>
2026-07-26 22:08:46 +0530

    booking serviceimpl

--- booking service/booking-service/src/main/java/com/rancho/controller/BookingController.java
+++ booking service/booking-service/src/main/java/com/rancho/controller/BookingController.java
@@ -1,10 +1,13 @@
 package com.rancho.controller;
 
+import com.rancho.dto.BookingRequest;
 import com.rancho.modal.Booking;
 import com.rancho.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +17,14 @@ public class BookingController {
 
     private final BookingService bookingService;
 
-    public ResponseEntity<Booking> createBooking(){
+    public ResponseEntity<Booking> createBooking(
+            @RequestParam Long salonId,
+            @RequestBody BookingRequest bookingRequest
+            ){
+
+
+        return null;
+
 
     }
 
